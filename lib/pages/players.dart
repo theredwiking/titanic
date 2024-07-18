f9ed476fb7379d807522e2e58ff33ad826d1fce7
Theredwiking <hjulmandn@gmail.com>
2024-07-18 17:53:01 +0200

    Players can be added now

--- lib/pages/players.dart
+++ lib/pages/players.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:titanic/db/questions.dart';
 
-class Players extends StatelessWidget {
+class Players extends StatefulWidget {
   final String mode;
   const Players({super.key, required this.mode});
+
+  @override
+  State createState() => _State();
+}
+
+class _State extends State<Players> {
+  final List<String> _players = [];
+  final TextEditingController nameCtl = TextEditingController();
+
+  void _addPlayer(String name) {
+    _players.add(name);
+  }
+
+  @override
+  void dispose() {
+    nameCtl.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,13 +49,70 @@ class Players extends StatelessWidget {
             style: TextButton.styleFrom(
                 backgroundColor: const Color(0xFFFCECC9),
                 minimumSize: const Size(263, 45)),
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      scrollable: true,
+                      title: const Text('Player'),
+                      content: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Form(
+                          child: Column(
+                            children: <Widget>[
+                              TextFormField(
+                                controller: nameCtl,
+                                decoration: const InputDecoration(
+                                  labelText: 'Name',
+                                  icon: Icon(Icons.account_box),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      actions: [
+                        ElevatedButton(
+                            child: const Text("Add"),
+                            onPressed: () {
+                              _addPlayer(nameCtl.text);
+                              nameCtl.clear();
+                              setState(() {});
+                              Navigator.pop(context);
+                            })
+                      ],
+                    );
+                  });
+            },
             child: const Text('Add player',
                 style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
                     color: Colors.black)),
           ),
+          Expanded(
+              child: ListView.builder(
+                  itemCount: _players.length,
+                  itemBuilder: (BuildContext ctxt, int index) {
+                    return Center(child: Text(_players[index],
+                        style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)));
+                  })),
+          TextButton(
+            style: TextButton.styleFrom(
+                backgroundColor: const Color(0xFFFCECC9),
+                minimumSize: const Size(263, 45)),
+            onPressed: () {},
+            child: const Text('Complete',
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black)),
+          ),
+          const SizedBox(height: 20,)
         ],
       )),
     );
